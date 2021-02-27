308ffe9bda8ec0625aa3f4a6213bb1ef87d4e836
federicodinucci <federico.dinucci14@gmail.com>
2021-02-27 15:19:02 -0300

    se modifica router para dejar base de proyecto

--- router.go
+++ router.go
@@ -1,18 +1,13 @@
 package main
 
 import (
-	"encoding/json"
-	"net/http"
-
 	"github.com/gorilla/mux"
 )
 
 func MainRouter() *mux.Router {
 	router := mux.NewRouter()
-	router.HandleFunc("/tasks", getTasks).Methods("POST")
+	AddHomeRoutes(router)
+	AddHelpRoutes(router)
+	AddTasksRoutes(router)
 	return router
-}
-
-func getTasks(w http.ResponseWriter, r *http.Request) {
-	json.NewEncoder(w).Encode("Task - POST")
 }
